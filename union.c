c4fd056ea334ecaaad337e66beec82f3ec53d4b8
siddhu <kodarisrisiddhardha@gmail.com>
2025-11-25 15:33:51 +0530

    Update union.c

--- union.c
+++ union.c
@@ -1,49 +1,30 @@
 #include <stdio.h>
 
-int main()
+int main() {
+    union student {
+        char name[25];
+        int rollno;
+        char course[7];
+        float marks;
+    };
 
-{
+    union student s;
 
-{
+    printf("Enter Name: \n");
+    scanf("%24s", s.name);
+    printf("Name: %s\n", s.name);
 
-union student
+    printf("Enter Roll No: \n");
+    scanf("%d", &s.rollno);
+    printf("Roll No: %d\n", s.rollno);
 
-char name[25];
+    printf("Enter Course: \n");
+    scanf("%6s", s.course);
+    printf("Course: %s\n", s.course);
 
-int rollno;
+    printf("Enter Marks: \n");
+    scanf("%f", &s.marks);
+    printf("Marks: %.2f\n", s.marks);
 
-char course [7];
-
-float marks;
-
-};
-
-union student s;
-
-printf("Enter Name : \n");
-
-scanf("%s", &s.name);
-
-printf("Name: %s\n",s.name);
-
-printf("Enter Roll No: \n");
-
-scanf("%d", &s.rollno);
-
-printf("Roll No: %d\n",s.rollno);
-
-printf("Enter Course : \n");
-
-scanf("%s", &s.course);
-
-printf("Course: %s\n",s.course);
-
-printf("Enter Marks : \n");
-
-scanf("%f",&s.marks);
-
-printf("Marks: %f\n",s.marks);
-
-return 0;
-
+    return 0;
 }
